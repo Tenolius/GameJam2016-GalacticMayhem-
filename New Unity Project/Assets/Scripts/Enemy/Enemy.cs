d06f7de6242b9cc8635a3a47eb4505b74b70381e
Trent You <tyou@ucdavis.edu>
2016-01-30 03:10:45 -0800

    Working turret attack functionality

--- New Unity Project/Assets/Scripts/Enemy/Enemy.cs
+++ New Unity Project/Assets/Scripts/Enemy/Enemy.cs
@@ -1,28 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyBehavior : MonoBehaviour {
+public class Enemy : MonoBehaviour {
 
 	Vector3 endPoint;
 	Vector3 startPoint;
 	float startTime;
 	float duration;
 	float moveSpeed;
+	private int damage = 5;
+	private bool movingForward;
 
 	// Use this for initialization
 	void Start () {
-		endPoint = new Vector3 (-45,5,0);
+		endPoint = new Vector3 (-70,5,0);
 		moveSpeed = 10.0f;
 		duration = 10.0f;
 		startPoint = transform.position;
 		startTime = Time.time;
+		movingForward = true;
 
 		//transform.position = Vector3 (0, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = Vector3.Lerp(startPoint, endPoint, (Time.time - startTime) / duration);
-		//transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+		if (movingForward) {
+			//transform.position = Vector3.Lerp (startPoint, endPoint, (Time.time - startTime) / duration);
+			transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+		}
 	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Turret") {
+			movingForward = false;
+		}
+
+	}
+
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void resumeMovement() {
+		movingForward = true;
+	}
+
+
 }
---
+++ New Unity Project/Assets/Turret.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+
+	private int health = 200;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Enemy") {
+			Debug.Log ("Enemy attacking turret");
+		}
+	}
+
+
+	void OnTriggerStay(Collider other) {
+		if (other.gameObject.tag == "Enemy") {
+			Enemy script = other.GetComponent<Enemy> ();
+			int damage = script.getDamage ();
+
+			if (health <= 0) {
+				Destroy (gameObject);
+				script.resumeMovement ();
+			} else {
+				health -= damage;
+			}
+		}
+	}
+
+	void OnParticleCollision(Collider other) {
+
+		if (other.gameObject.tag == "Enemy") {
+			Debug.Log ("Shot enemy");
+		}
+	}
+
+
+
+
+
+
+}
