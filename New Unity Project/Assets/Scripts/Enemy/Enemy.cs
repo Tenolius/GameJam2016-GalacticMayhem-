b6b6773fa42e60bbc65262eedf05e85242888be5
ggu <gabriel.uribe@alumni.ubc.ca>
2016-01-30 01:42:22 -0800

    Updated game objects

---
+++ New Unity Project/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	Vector3 endPoint;
+	Vector3 startPoint;
+	float startTime;
+	float duration;
+	float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		endPoint = new Vector3 (-45,5,0);
+		moveSpeed = 10.0f;
+		duration = 10.0f;
+		startPoint = transform.position;
+		startTime = Time.time;
+
+		//transform.position = Vector3 (0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(startPoint, endPoint, (Time.time - startTime) / duration);
+		//transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+	}
+}
