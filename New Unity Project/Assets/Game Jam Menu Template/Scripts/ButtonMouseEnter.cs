8df67eca305d9748f4b5364c11545d9095829a50
aihchen <angelaichen87@gmail.com>
2016-01-30 20:22:15 -0800

    menu sound

---
+++ New Unity Project/Assets/Game Jam Menu Template/Scripts/ButtonMouseEnter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class ButtonMouseEnter : MonoBehaviour {
+
+    //Create the buttons in Editor and drop them into the script's component reference.
+    public UnityEngine.UI.Button start;
+    public UnityEngine.UI.Button options;
+    public UnityEngine.UI.Button quit;
+
+    public void playSound()
+    {
+        
+    }
+
+}
+
