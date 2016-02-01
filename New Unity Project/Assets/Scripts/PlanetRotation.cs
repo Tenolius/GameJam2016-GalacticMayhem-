dc4bd38116470ce7f4806a8cf819506a0e963521
Rafael Alfaro Martinez <rafe.alfaro@outlook.com>
2016-01-31 16:27:32 -0800

    polishing stuff

---
+++ New Unity Project/Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetRotation : MonoBehaviour {
+    public float RotationSpeed = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RotatePlanet();
+	}
+
+    void RotatePlanet(){
+        transform.Rotate(Vector3.up * (RotationSpeed * Time.deltaTime));
+    }
+}
---
+++ New Unity Project/Assets/Shader/RimLight.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e4da31b44c8005a2579fa23b75608855563dffd9cd02afd47395547c50c42eac
+size 22636
